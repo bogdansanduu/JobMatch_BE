4497668b932db9af94303f64c4b762de94e701b8
Bogdan <sandubogdan2001@gmail.com>
2024-04-08 22:41:05 +0300

    secured sockets using passport js and jwt, ONLY ON CONNECTION

--- src/common/socket/socket-io.ts
+++ src/common/socket/socket-io.ts
@@ -1,13 +1,36 @@
 import * as http from 'http';
 import { Server } from 'socket.io';
+import passport from 'passport';
 
 import { app } from '../../server';
 import { dataSource } from '../../database/dataSource';
 import { User } from '../../user/entities/user.entity';
 
-const ioSocket = new Server(http.createServer(app));
+const ioSocket = new Server(http.createServer(app), {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 const userRepository = dataSource.getRepository<User>(User);
 
+ioSocket.use((socket, next) => {
+  if (socket.request.headers.authorization) {
+    passport.authenticate('jwt', { session: false }, (err, payload) => {
+      if (err || !payload) {
+        return next(new Error('Authentication error'));
+      }
+
+      next();
+    })(socket.request, {}, next);
+
+    return;
+  }
+
+  next(new Error('Authentication error'));
+});
+
+//updates the socket id of the user in the database
 ioSocket.use(async (socket, next) => {
   const userId = parseInt(socket.handshake.query.userId as string);
 
