ccfabd3c5decaac734a27e2664dd5deba775023e
Bogdan <sandubogdan2001@gmail.com>
2024-06-21 21:36:34 +0300

    railway

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { app, logger } from './server';
 import ioSocket from './common/socket/socket-io';
 import { getEnvVar } from './common/utils/envConfig';
 
-const appPort = getEnvVar<number>('APP_PORT', 'number');
+const appPort = getEnvVar<number>('PORT', 'number');
 const socketPort = getEnvVar<number>('SOCKET_PORT', 'number');
 
-app.listen(appPort, () => {
+app.listen(appPort, '0.0.0.0', () => {
   logger.info(`Server is running on port ${appPort}`);
 });
 
