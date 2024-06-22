8febdb9512430d9ea4bfff8839b84bc041b8ee13
Bogdan <sandubogdan2001@gmail.com>
2024-06-22 18:18:50 +0300

    same port socket and http

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import 'reflect-metadata';
 import { app, logger } from './server';
 import ioSocket from './common/socket/socket-io';
 import { getEnvVar } from './common/utils/envConfig';
+import http from 'http';
 
 const appPort = getEnvVar<number>('PORT', 'number');
-const socketPort = getEnvVar<number>('SOCKET_PORT', 'number');
+// const socketPort = getEnvVar<number>('SOCKET_PORT', 'number');
 
-app.listen(appPort, '0.0.0.0', () => {
+const server = http.createServer(app);
+ioSocket.listen(server);
+
+server.listen(appPort, '0.0.0.0', () => {
   logger.info(`Server is running on port ${appPort}`);
 });
-
-ioSocket.listen(socketPort);
