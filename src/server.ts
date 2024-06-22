7def25db18abbf4ca14a0affe039cdd61e6a8970
Bogdan <sandubogdan2001@gmail.com>
2024-06-22 20:01:48 +0300

    rec sys trial

--- src/server.ts
+++ src/server.ts
@@ -51,7 +51,7 @@ app.use(helmet());
 
 // //allow cors
 const corsOptions: CorsOptions = {
-  origin: getEnvVar<string>('CORS_ORIGIN', 'string'), // Allow your React app's origin
+  origin: [getEnvVar<string>('CORS_ORIGIN', 'string'), 'http://localhost:8000'], // Allow your React app's origin
   // credentials: true, // Access-Control-Allow-Credentials: true (if you need to send cookies or HTTP authentication)
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   // allowedHeaders: ['Authorization', 'Content-Type'],
