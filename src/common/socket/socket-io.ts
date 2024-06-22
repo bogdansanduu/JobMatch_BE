8423fbf09e0f8c98630e5f8c46651e058af0fad0
Bogdan <sandubogdan2001@gmail.com>
2024-06-22 17:16:19 +0300

    socket tests

--- src/common/socket/socket-io.ts
+++ src/common/socket/socket-io.ts
@@ -1,11 +1,11 @@
 import * as http from 'http';
 import { Server } from 'socket.io';
-import passport from 'passport';
+// import passport from 'passport';
 
 import { app } from '../../server';
-import { dataSource } from '../../database/dataSource';
-import { User } from '../../user/entities/user.entity';
-import { HttpException } from '../exceptions/http.exception';
+// import { dataSource } from '../../database/dataSource';
+// import { User } from '../../user/entities/user.entity';
+// import { HttpException } from '../exceptions/http.exception';
 
 const ioSocket = new Server(http.createServer(app), {
   cors: {
@@ -13,49 +13,49 @@ const ioSocket = new Server(http.createServer(app), {
     methods: ['GET', 'POST'],
   },
 });
-const userRepository = dataSource.getRepository<User>(User);
+// const userRepository = dataSource.getRepository<User>(User);
 
-ioSocket.use((socket, next) => {
-  if (socket.request.headers.authorization) {
-    passport.authenticate('jwt', { session: false }, (err, payload) => {
-      if (err || !payload) {
-        return next(new HttpException('Authentication error', 401));
-      }
-
-      next();
-    })(socket.request, {}, next);
-
-    return;
-  }
-
-  next(new HttpException('Authentication error', 401));
-});
-
-//updates the socket id of the user in the database
-ioSocket.use(async (socket, next) => {
-  const userId = parseInt(socket.handshake.query.userId as string);
-
-  if (!userId) {
-    next();
-    return;
-  }
-
-  const user = await userRepository.findOne({
-    where: {
-      id: userId,
-    },
-  });
-
-  if (!user) {
-    next();
-    return;
-  }
-
-  user.socketId = socket.id;
-  await userRepository.save(user);
-
-  next();
-});
+// ioSocket.use((socket, next) => {
+//   if (socket.request.headers.authorization) {
+//     passport.authenticate('jwt', { session: false }, (err, payload) => {
+//       if (err || !payload) {
+//         return next(new HttpException('Authentication error', 401));
+//       }
+//
+//       next();
+//     })(socket.request, {}, next);
+//
+//     return;
+//   }
+//
+//   next(new HttpException('Authentication error', 401));
+// });
+//
+// //updates the socket id of the user in the database
+// ioSocket.use(async (socket, next) => {
+//   const userId = parseInt(socket.handshake.query.userId as string);
+//
+//   if (!userId) {
+//     next();
+//     return;
+//   }
+//
+//   const user = await userRepository.findOne({
+//     where: {
+//       id: userId,
+//     },
+//   });
+//
+//   if (!user) {
+//     next();
+//     return;
+//   }
+//
+//   user.socketId = socket.id;
+//   await userRepository.save(user);
+//
+//   next();
+// });
 
 ioSocket.on('connection', (socket) => {
   console.log(`User connected with socket id ${socket.id}`);
